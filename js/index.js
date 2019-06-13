2649f8788c7692a5f2cf907d258c324c53d6dcf3
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-06-13 16:00:54 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 // your code here
 
-
+function showRepositories() {
+  console.log(this.responseText)
+  let repoList = '<ul>'
+  for (var i = 0; i < this.responseText.length; i++) {
+    repoList += '<li>' + this.responseText[i]['name'] + '</li>'
+  }
+  repolist += '</ul>'
+  
+  document.getElementById('repositories').innerHTML = repoList
+}
 
 
 function getRepositories() {
