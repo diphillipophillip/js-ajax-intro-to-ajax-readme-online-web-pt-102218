9c535ec40a7c1cdb81f2372e9f2957c01891f546
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-06-13 16:04:01 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,14 +1,5 @@
 // your code here
 
-function showRepositories() {
-  var repos = JSON.parse(this.responseText)
-  console.log(repos)
-  const repoList = '<ul>'
-  const repoList = `<ul>${repos
-    .map(r => '<li>' + r.name + '</li>')
-    .join('')}</ul>`;
-  document.getElementById('repositories').innerHTML = repoList;
-}
 
 
 function getRepositories() {
