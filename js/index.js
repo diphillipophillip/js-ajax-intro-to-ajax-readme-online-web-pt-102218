408400af09bdced8c47f84d37b558bba8965fdf6
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-06-13 15:51:22 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 // your code here
 function getRepositories() {
   const req = new XMLHttpRequest()
+  req.addEventListener('load', showRepositories)
   req.open('GET', 'https://api.github.com/users/octocat/repos')
   req.send()
+}
+
+function showRepositories() {
+  console.log(this.responseText)
 }
